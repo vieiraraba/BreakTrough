95dbcc803a28b39403765f3c126050133dfc6c85
Ricardo Vieira <vieiraraba@gmail.com>
2022-05-11 11:25:23 +0200

    adding first-row to the js and css

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,7 @@ const workingArea = document.getElementById("workingArea");
 const gridContainer = ['','','','','','','','','','','','','','',''];
 
 gridContainer.map(function(){
-
-    let rowContainer = document.createElement("div");
-    workingArea.appendChild(rowContainer);
-    rowContainer.setAttribute("class", "first-row");
+    let rowContainer1 = document.createElement("div");
+    workingArea.appendChild(rowContainer1);
+    rowContainer1.setAttribute("class", "first-row");
 });
